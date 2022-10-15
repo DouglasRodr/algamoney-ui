20316a4264eca64cdf6d03fd5c9fd9b7899b6f5d
DouglasRodr <wandersondouglasr@gmail.com>
2022-10-14 23:46:22 -0300

    Módulo 14. Módulos do Angular

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-pessoas-pesquisa',
   templateUrl: './pessoas-pesquisa.component.html',
-  styleUrls: ['./pessoas-pesquisa.component.css'],
+  styleUrls: ['./pessoas-pesquisa.component.css']
 })
 export class PessoasPesquisaComponent {
   pessoas = [
@@ -11,13 +11,13 @@ export class PessoasPesquisaComponent {
       nome: 'Manoel Pinheiro',
       cidade: 'Uberlândia',
       estado: 'MG',
-      ativo: true,
+      ativo: true
     },
     {
       nome: 'Sebastião da Silva',
       cidade: 'São Paulo',
       estado: 'SP',
-      ativo: false,
+      ativo: false
     },
     { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
     { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
@@ -25,8 +25,8 @@ export class PessoasPesquisaComponent {
       nome: 'Vilmar Andrade',
       cidade: 'Rio de Janeiro',
       estado: 'RJ',
-      ativo: false,
+      ativo: false
     },
-    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true },
-  ];
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
+  ]
 }
